af1ccf45cd7eeb9ce650d46e93aff54ea619c9f0
gaganjain280 <gaganjain280@gmail.com>
2019-12-20 00:31:10 +0530

    first co

---
+++ build/precache-manifest.7db2306328077f929af0f1c867f1cb68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748080ff52026ad249017e8355b738e7",
+    "url": "/gagancodingkart/wooreact/index.html"
+  },
+  {
+    "revision": "5c9397962c1e4ed17840",
+    "url": "/gagancodingkart/wooreact/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0678aee6062e8b9ecf5d",
+    "url": "/gagancodingkart/wooreact/static/css/main.7a2cb26a.chunk.css"
+  },
+  {
+    "revision": "5c9397962c1e4ed17840",
+    "url": "/gagancodingkart/wooreact/static/js/2.b23bbe30.chunk.js"
+  },
+  {
+    "revision": "ea538b5324860afec8786da620cef98b",
+    "url": "/gagancodingkart/wooreact/static/js/2.b23bbe30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0678aee6062e8b9ecf5d",
+    "url": "/gagancodingkart/wooreact/static/js/main.9d2758b5.chunk.js"
+  },
+  {
+    "revision": "8dd058db18bf43ee20a1",
+    "url": "/gagancodingkart/wooreact/static/js/runtime-main.e51be4fb.js"
+  }
+]);
